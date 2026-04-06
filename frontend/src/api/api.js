a27824fdf0fbbbc56c389ef5f91b9363b59d9b76
mnaveen1702 <mnaveen1702@gmail.com>
2026-04-06 09:09:47 +0530

    fixed API base URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://habit-traker-awil.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 })
 
